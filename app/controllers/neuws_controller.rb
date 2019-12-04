147a22e0b0be42247ef6d9c146525a73192b664c
kileys-aafc <simon.kiley@canada.ca>
2019-12-04 15:16:26 -0500

    fixed datetime value for sorting

--- app/controllers/neuws_controller.rb
+++ app/controllers/neuws_controller.rb
@@ -1,8 +1,8 @@
 class NeuwsController < ApplicationController
   
   def publish
-    @neuws = Neuw.where('created_at > "2010"').order('created_at DESC')
-    @oldNeuws = Neuw.where('created_at < "2010"').order('created_at ASC')
+    @neuws = Neuw.where('created_at > "2010-01-01"').order('created_at DESC')
+    @oldNeuws = Neuw.where('created_at < "2010-01-01"').order('created_at ASC')
     @breadcrumbsArray = Array.new
     if params[:rootdir] == "cansis" then @lang = "en" else @lang = "fr" end
     respond_to do |format|
