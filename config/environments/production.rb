f7f30703c5b06167c59d917aac6ad0080f4b78ff
Simon Kiley <simon.kiley.gis@gmail.com>
2021-02-03 12:03:14 -0500

    set asset compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Website::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Set to true Feb. 2020, to allow leaflet webmap to load on rails production mode
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
